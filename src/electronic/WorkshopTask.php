e02661d7435b60da23eec773cc43c913bc800fef
zhoushun33 <1071207477@qq.com>
2019-10-10 11:03:49 +0800

    10191010 11:03
    电子看板bug

--- src/electronic/WorkshopTask.php
+++ src/electronic/WorkshopTask.php
@@ -2,16 +2,18 @@
  	require("../../conn.php");
 //	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
  	$ret_data=array();
+ 	$i=0;
 	$arr = $_POST["arr"];
- 	$sql="SELECT route,name,figure_number,schedule_date,otime,todocount from `workshop_k` where isfinish = '2' and route in $arr ORDER BY id DESC limit 10";
+ 	$sql="SELECT route,name,figure_number,schedule_date,otime,todocount from `workshop_k` where isfinish = '2' and route in ('K','K坡') ORDER BY id DESC limit 10";
  	$result = $conn->query($sql);
  	while ($row = $result->fetch_assoc()) {
- 		$ret_data["Serial"] = $row["route"];
- 		$ret_data["name"] = $row["name"];
- 		$ret_data["figure_number"] = $row["figure_number"];
- 		$ret_data["time"] = $row["schedule_date"];
- 		$ret_data["finished"] = $row["otime"];
- 		$ret_data["todocount"] = $row["todocount"];
+ 		$ret_data[$i]["Serial"] = $row["route"];
+ 		$ret_data[$i]["name"] = $row["name"];
+ 		$ret_data[$i]["figure_number"] = $row["figure_number"];
+ 		$ret_data[$i]["time"] = $row["schedule_date"];
+ 		$ret_data[$i]["finished"] = $row["otime"];
+ 		$ret_data[$i]["todocount"] = $row["todocount"];
+ 		$i++;
 	}
  	$ret_data["success"] = 'success';
 	$conn->close();
